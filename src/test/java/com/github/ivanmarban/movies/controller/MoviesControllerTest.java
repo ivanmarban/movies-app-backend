0955e65c4e31df994d7adc51b8eb1e8f0c1501cd
Ivan Marban <ivan.marban@gmail.com>
2021-07-22 16:36:27 +0200

    test: added more unit tests

--- src/test/java/com/github/ivanmarban/movies/controller/MoviesControllerTest.java
+++ src/test/java/com/github/ivanmarban/movies/controller/MoviesControllerTest.java
@@ -14,12 +14,12 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.willDoNothing;
+import static org.mockito.BDDMockito.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @DisplayName("MoviesController Unit Tests")
@@ -52,6 +52,23 @@ public class MoviesControllerTest {
     }
 
     @Test
+    @DisplayName("Create movie. Throws exception.")
+    public void createMovieThrowsException() throws Exception {
+
+        Movie movie = Movie.builder().title("2001: A Space Odyssey").year("1968").rated("G").runtime("160 min").genre("Mystery, Sci-Fi")
+            .director("Stanley Kubrick").build();
+
+        willThrow(new RuntimeException("horror")).given(moviesRepository).save(movie);
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .post("/api/movies")
+            .content(asJsonString(movie))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError());
+    }
+
+    @Test
     @DisplayName("Get all movies")
     public void getMovies() throws Exception {
 
@@ -59,10 +76,37 @@ public class MoviesControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders
             .get("/api/movies")
+            .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
             .andExpect(MockMvcResultMatchers.jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    @DisplayName("Get all movies. Not found.")
+    public void getMoviesNotFound() throws Exception {
+
+        given(moviesRepository.findAll()).willReturn(Collections.emptyList());
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/movies")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("Get all movies. Throws exception.")
+    public void getMoviesThrowsException() throws Exception {
+
+        willThrow(new RuntimeException("horror")).given(moviesRepository).findAll();
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/movies")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isInternalServerError());
     }
 
     @Test
@@ -78,6 +122,7 @@ public class MoviesControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
             .get("/api/movies")
             .param("title", "Space")
+            .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
@@ -95,9 +140,23 @@ public class MoviesControllerTest {
 
         mockMvc.perform(MockMvcRequestBuilders
             .get("/api/movies/{id}", "60f6cd5366949871c415d9fd")
+            .contentType(MediaType.APPLICATION_JSON)
             .accept(MediaType.APPLICATION_JSON))
             .andExpect(status().isOk())
             .andExpect(MockMvcResultMatchers.jsonPath("$.id").value("60f6cd5366949871c415d9fd"));
+    }
+
+    @Test
+    @DisplayName("Get movie by ID. Not Found.")
+    public void getMovieByIdNotFound() throws Exception {
+
+        given(moviesRepository.findById("60f6cd5366949871c415d9fd")).willReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .get("/api/movies/{id}", "60f6cd5366949871c415d9fd")
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
     }
 
     @Test
@@ -122,6 +181,22 @@ public class MoviesControllerTest {
     }
 
     @Test
+    @DisplayName("Update movie. Not Found.")
+    public void updateMovieNotFound() throws Exception {
+        Movie movie = Movie.builder().id("60f6cd5366949871c415d9fd").title("2001: A Space Odyssey").year("1968").rated("G").runtime("160 min")
+            .genre("Mystery, Sci-Fi").director("Stanley Kubrick").build();
+
+        given(moviesRepository.findById("60f6cd5366949871c415d9fd")).willReturn(Optional.empty());
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .put("/api/movies/{id}", "60f6cd5366949871c415d9fd")
+            .content(asJsonString(movie))
+            .contentType(MediaType.APPLICATION_JSON)
+            .accept(MediaType.APPLICATION_JSON))
+            .andExpect(status().isNotFound());
+    }
+
+    @Test
     @DisplayName("Delete movie")
     public void deleteMovie() throws Exception {
 
@@ -134,6 +209,18 @@ public class MoviesControllerTest {
     }
 
     @Test
+    @DisplayName("Delete movie. Throws exception.")
+    public void deleteMovieThrowsException() throws Exception {
+
+        willThrow(new RuntimeException("horror")).given(moviesRepository).deleteById("60f6cd5366949871c415d9fd");
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .delete("/api/movies/{id}", "60f6cd5366949871c415d9fd"))
+            .andExpect(status().isInternalServerError());
+
+    }
+
+    @Test
     @DisplayName("Delete all movies")
     public void deleteAllMovies() throws Exception {
 
@@ -142,6 +229,17 @@ public class MoviesControllerTest {
         mockMvc.perform(MockMvcRequestBuilders
             .delete("/api/movies"))
             .andExpect(status().isNoContent());
+    }
+
+    @Test
+    @DisplayName("Delete all movies. Throws exception.")
+    public void deleteAllMoviesThrowsException() throws Exception {
+
+        willThrow(new RuntimeException("horror")).given(moviesRepository).deleteAll();
+
+        mockMvc.perform(MockMvcRequestBuilders
+            .delete("/api/movies"))
+            .andExpect(status().isInternalServerError());
     }
 
     public List<Movie> movieList() {
